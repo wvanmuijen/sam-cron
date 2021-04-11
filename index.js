00ab7d28b34425f3f12d3ee97cf80f807f13bd10
chirag prajapati <chirag@samcomtechnobrains.com>
2021-04-11 23:13:12 +0530

    create send reminder to company api

--- index.js
+++ index.js
@@ -8,10 +8,42 @@ const cron = require('node-cron');
 
 const Promise = require('bluebird');
 
-const limit = 1;
+const limit = 10;
 const offset = 0;
 
-/*const getCompanies = (limit, offset) => new Promise((resolve, reject) => {
+/**
+ * send Reminders To Companies
+ */
+exports.sendRemindersToCompanies = async (limit, offset) => {
+  try {
+
+    // get getCompanies
+    let fetchCompany = await getCompanies(limit, offset);
+
+    if (fetchCompany.rows && fetchCompany.rows.length > 0) {
+      return Promise.mapSeries(fetchCompany.rows, async function (item, key) {
+        let obj = { companyId: item.id }
+        return sqsService.sendSQSMessage(JSON.stringify(obj))
+          .then((sqsRes) => {
+            console.log('Success: ', sqsRes.MessageId);
+            return sqsRes.MessageId
+          })
+          .catch((err) => {
+            console.log('Sqs service Error: ', err);
+          });
+      }).then(function(result) {
+        // This will run after the last step is done
+        console.log("Done!" ,result);
+        return module.exports.sendRemindersToCompanies(limit, offset + limit);
+      });
+    }
+
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+const getCompanies = (limit, offset) => new Promise((resolve, reject) => {
   
   const companyQuery = databaseProvider
     .knex('companies')
@@ -20,72 +52,17 @@ const offset = 0;
     .offset(offset);
 
   return resolve(databaseProvider.executeQuery(companyQuery));  
-  
+
 });
 
-async function sendRemindersToCompanies() {
-  let fetchCompany = await getCompanies(limit, offset);
-  console.log('quote>>>', fetchCompany.rows);
-  if (fetchCompany.rows && fetchCompany.rows.length > 0) {
-    offset = limit
-    limit = limit + 1
-  }
-}*/
-
-const getCompanies = () => {
-  const companyQuery = databaseProvider
-    .knex('companies')
-    .select('id')
-    .limit(limit)
-    .offset(offset);
-
-  return databaseProvider.executeQuery(companyQuery);
-};
-
-console.log('cron started');
-
 if (process.env.CRON_VALUE) {
-  // cron.schedule(process.env.CRON_VALUE, () => {
-  //   getCompanies()
-  //     .then((res) => console.log(res.rows))
-  //     .then(() => {
-  //       console.log('cron executed');
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //     });
-  // });
-} else {
-  console.log('cron runnig wihout cron value.');
-  
-  //sendRemindersToCompanies();
-  // let companies =  getCompanies(limit, offset)
-  // console.log('companies>>>' ,companies);
-  // sendRemindersToCompanies().then( async (res) => {
-  //   console.log('cron executed without cron value.', JSON.stringify(res.rows));
-  // })
-
-  getCompanies()
-    .then(async (res) => {
-      console.log('cron executed without cron value.', JSON.stringify(res.rows));
-      if (res.rows && res.rows.length > 0) {
-        await Promise.each(res.rows, async function (item, key) {
-          let obj = { companyId: item.id }
-          await sqsService.sendSQSMessage(JSON.stringify(obj))
-            .then((sqsRes) => {
-              console.log('Success: ', sqsRes.MessageId);
-            })
-            .catch((err) => {
-              console.log('Sqs service Error: ', err);
-            });
-        })
-        console.log('Done');
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+  cron.schedule(process.env.CRON_VALUE, () => {
+    exports.sendRemindersToCompanies(limit, offset);
+  });
+}else{
+  exports.sendRemindersToCompanies(limit, offset);
 }
+
 
 
 
